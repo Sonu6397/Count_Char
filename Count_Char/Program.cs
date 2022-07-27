5f3abe169afde69d316ba3df81dda4a25e9e5f04
admin <admin@CHETUIWK1860>
2022-07-27 15:46:48 +0530

    Add project files.

---
+++ Count_Char/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public class Example
+{
+    public static void Main()
+    {
+        string str = "sweet";
+        char ch = 'e';
+
+        int freq = str.Count(f => (f == ch));
+        Console.WriteLine(freq);
+        Console.ReadLine();
+    }
+}
